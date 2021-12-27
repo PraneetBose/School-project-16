86af03216409d3de3c755f41b773e0f7512a7cab
Praneet Bose <94593299+PraneetBose@users.noreply.github.com>
2021-12-27 05:15:25 -0500

    Create Project -16.py

---
+++ Project -16.py
@@ -0,0 +1,12 @@
+# Python program to display the maximum and minimum number from the list.
+
+# min()
+# printing the minimum of 4,12,43.3,19,100
+print("Minimum of 4,12,43.3,19 and 100 is : ",end="")
+print (min( 4,12,43.3,19,100 ) )
+
+
+# max()
+# printing the maximum of 4,12,43.3,19,100
+num=str(input("Enter 5 numbers -"))
+print (max(num ) )
